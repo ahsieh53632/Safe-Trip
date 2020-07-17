f2be3400d0eb7d7a2b53cd84afdb0577cbbda5cc
Alex Hsieh <38302360+ahsieh53632@users.noreply.github.com>
2020-07-17 08:44:58 -0400

    login_page

--- api/routes/Login.js
+++ api/routes/Login.js
@@ -1,8 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql')
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '!Alex3638',
+  database: 'safe-trip'
+});
+
+
+
+con.connect(function(err){
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('connected')
+    }
+  });
+
 
 /* GET home page. */
-router.post('/auth', function(req, res, next) {
+router.post('/auth', function(req, res) {
+    
   res.render('index', { title: 'Express' });
 });
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,27 @@
 import React from "react"
 import { Link } from "gatsby"
+import React from "react";
+import {createMemoryHistory} from "history";
+import {Route, Router, Switch} from "react-router-dom";
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import LoginPage from "./LoginPage/LoginPage.jsx";
+let hist = createMemoryHistory();
+
+
+
+export default () => (
+  <Router history={hist}>
+    <Switch>
+      <Route path="/" component={LoginPage} />
+      {/* <Route path="/schedule-page" component={SchedulePage} />
+      <Route path="/map-page" component={MapPage} />
+      <Route path="/main-page" component={MainPage} /> */}
+    </Switch>
+  </Router>
+);
 
 const IndexPage = () => (
   <Layout>
