4625680e79a5169e08891f186fda287300d7e864
real_andy_loud <leokou42@gmail.com>
2020-07-19 07:30:40 +0800

    g

--- src/pages/AddID/AddIDPage.jsx
+++ src/pages/AddID/AddIDPage.jsx
@@ -9,6 +9,7 @@ import Header from "components/Header/Header.jsx";
 import HeaderLinks from "components/Header/HeaderLinks.jsx";
 import Button from "../../components/CustomButtons/Button.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
+import NoIconHeader from "components/Header/NoIconHeader.jsx";
 
 import loginPageStyle from "../../assets/jss/material-kit-react/views/loginPage.jsx"
 import withStyles from "@material-ui/core/styles/withStyles";
@@ -31,7 +32,7 @@ class AddIDPage extends Component{
             <div>
             <Header
             brand="Safe-Trip"
-            rightLinks={<HeaderLinks />}
+            rightLinks={<NoIconHeader />}
             fixed
             changeColorOnScroll={{
             height: 100,
