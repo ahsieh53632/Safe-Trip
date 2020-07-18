e02325a9806eb26e56398c6d1153eebebf47e2de
Alex Hsieh <38302360+ahsieh53632@users.noreply.github.com>
2020-07-18 08:37:25 -0400

    qr code api call

--- src/pages/scanCode.js
+++ src/pages/scanCode.js
@@ -1,19 +1,7 @@
 
-// customQueryStringComponent.js
+import React from "react";
+import ScanQR from "./scanCode.jsx";
 
-import React from "react"
-import PropTypes from "prop-types"
-import withLocation from "./withLocation"
-
-const ScanQR = ({ search }) => {
-    console.log('hi');
-  const { custom } = search;
-  console.log(custom);
-  return <div><h1>123</h1><p>Custom Value: {custom}</p></div>
-}
-
-ScanQR.propTypes = {
-  search: PropTypes.object,
-}
-
-export default withLocation(ScanQR)
+export default () => (
+  <ScanQR/>
+);
