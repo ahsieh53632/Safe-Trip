e32f269ccd6bff8e40f63a63c88521997d4ee55b
Alex Hsieh <38302360+ahsieh53632@users.noreply.github.com>
2020-07-18 18:46:52 -0400

    update alert

---
+++ src/components/Header/NoIconHeader.jsx
@@ -0,0 +1,53 @@
+/*eslint-disable*/
+import React from "react";
+// react components for routing our app without refresh
+import { Link } from "gatsby";
+
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Tooltip from "@material-ui/core/Tooltip";
+
+// @material-ui/icons
+import { Apps, Schedule, Home, Map } from "@material-ui/icons";
+
+// React icons
+import { FaTwitter, FaFacebook, FaInstagram } from "react-icons/fa";
+
+// core components
+import CustomDropdown from "components/CustomDropdown/CustomDropdown.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+
+import headerLinksStyle from "assets/jss/material-kit-react/components/headerLinksStyle.jsx";
+
+import ScheduleDialog from "pages/SchedulePage/ScheduleDialog.jsx";
+import MapDialog from "pages/MapPage/MapDialog.jsx";
+import WarningSign from "pages/MainPage/WarningSign.jsx"
+
+function HeaderLinks({ ...props }) {
+    const { classes } = props;
+    return (
+        <List className={classes.list}>
+            {/* <ListItem className={classes.listItem}>
+                <Link to="/main-page" className={classes.navLink}>
+                    <Home className={classes.icons} />
+                    Home
+                </Link>
+            </ListItem> */}
+
+            {/* <ListItem className={classes.listItem}>
+                <ScheduleDialog />
+            </ListItem> */}
+            {/* <ListItem className={classes.listItem}>
+                <MapDialog />
+            </ListItem> */}
+
+            <ListItem className={classes.listItem}>
+            </ListItem>
+            
+        </List>
+    );
+}
+
+export default withStyles(headerLinksStyle)(HeaderLinks);
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -8,6 +8,7 @@ import AddID from "./AddID";
 
 import Header from "components/Header/Header.jsx";
 import HeaderLinks from "components/Header/HeaderLinks.jsx";
+import NoIconHeader from "components/Header/NoIconHeader.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
 import loginPageStyle from "../../assets/jss/material-kit-react/views/loginPage.jsx"
 import withStyles from "@material-ui/core/styles/withStyles";
@@ -24,21 +25,56 @@ class MainPage extends Component{
           Check: false,
           MyQRcode: false,
           UpadteInfo: false,
+          alert: false,
       };
   }
 
+  componentDidMount () {
+    fetch('http://localhost:3000/CheckPage/check', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "account": this.cache_account,
+        })
+        })
+        .then(res => res.json())
+        .then(d => {
+            if (d.alert) {
+              this.state.alert = true
+            }
+      })
+  }
+
+ 
+
   render() {
+    if (this.state.alert) {
+      var header = 
+        <Header brand="Safe-Trip"
+              rightLinks={<HeaderLinks />}
+              fixed
+              changeColorOnScroll={{
+              height: 100,
+              color: "white",
+            }}/>
+    } else {
+      var header = 
+        <Header
+              brand="Safe-Trip"
+              rightLinks={<NoIconHeader />}
+              fixed
+              changeColorOnScroll={{
+              height: 100,
+              color: "white",
+            }} />
+    }
+
       return(
         <div>
-          <Header
-            brand="Safe-Trip"
-            rightLinks={<HeaderLinks />}
-            fixed
-            changeColorOnScroll={{
-            height: 100,
-            color: "white",
-          }}/>
-
+            {header}
         <div
           style={{
             backgroundImage: "url(" + image + ")",
