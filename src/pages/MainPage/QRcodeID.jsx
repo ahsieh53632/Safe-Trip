e9ab354d81f4260d19326548c541f4935ea3eb1d
106303551 <66413547+106303551@users.noreply.github.com>
2020-07-18 20:40:11 +0800

    56

--- src/pages/MainPage/QRcodeID.jsx
+++ src/pages/MainPage/QRcodeID.jsx
@@ -3,25 +3,20 @@ import {Link, navigate} from "gatsby";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Button from "../../components/CustomButtons/Button.jsx";
 import MyQRcode from "qrcode.react";
-// 地名QRcode;
+// 生成人名QRcode;
 import loginPageStyle from "../../assets/jss/material-kit-react/views/loginPage.jsx"
 
 
 
-class QRcodeplace extends Component{
+class QRcodeID extends Component{
     constructor(props) {
         super(props);
-        this.state={
-
-            
-
-        }
       }
 
     render() {
            return (
                 <div>
-                    <MyQRcode value={this.props.sendplace} ></MyQRcode>
+                    <MyQRcode value={this.props.sendID} ></MyQRcode>
                 </div>
             )
 
@@ -30,4 +25,4 @@ class QRcodeplace extends Component{
     }
 }
 
-export default withStyles(loginPageStyle)(QRcodeplace);
+export default withStyles(loginPageStyle)(QRcodeID);
