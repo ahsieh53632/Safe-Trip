36188f46fbf18b431442148950e2bb4fb617f72c
Alex Hsieh <38302360+ahsieh53632@users.noreply.github.com>
2020-07-19 03:19:03 -0400

    update api

--- api/app.js
+++ api/app.js
@@ -48,7 +48,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/LoginPage', LoginRouter);
 app.use('/CheckPage', CheckRouter);
