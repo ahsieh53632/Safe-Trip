91599012335c1099d6fdc2b421299ee41117fcf8
Alex Hsieh <38302360+ahsieh53632@users.noreply.github.com>
2020-07-21 11:36:56 -0400

    fix 2

--- src/pages/createLoc.jsx
+++ src/pages/createLoc.jsx
@@ -18,7 +18,7 @@ const ScanQRLocation = ({ search }) => {
     objs = JSON.parse(info);
   }
   console.log(objs)
-var type = objs.type;
+var Type = objs.type;
 var locationName = objs.locationName;
 var address = objs.address;
 var account = objs.account;
@@ -54,7 +54,7 @@ var Date = objs.Date;
           })
     } 
   }
-return <div><h1>type:{type}</h1><p>yourid: {account}</p><p>locationid: {locationName}</p></div>
+return <div><h1>type:{Type}</h1><p>yourid: {account}</p><p>locationid: {locationName}</p></div>
 }
 
 ScanQRLocation.propTypes = {
